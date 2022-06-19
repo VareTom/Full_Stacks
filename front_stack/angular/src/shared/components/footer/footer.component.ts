0a3252a07aeb92254fbc0b7f16b9c6878b06ffdd
VareTom <tomvarewyck98@gmail.com>
2022-06-19 22:24:29 +0200

    Styling footer

--- front_stack/angular/src/shared/components/footer/footer.component.ts
+++ front_stack/angular/src/shared/components/footer/footer.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
+// Enums
+import { LanguageCodes } from 'src/shared/enums/LanguageCodes';
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
-  constructor() { }
-
+  isDarkMode: boolean = true;
+  
+  LanguageCodes = LanguageCodes;
+  
+  constructor(public translate: TranslateService) { }
+  
   ngOnInit(): void {
+  }
+  
+  onSwitchTheme(): void {
+    this.isDarkMode = !this.isDarkMode;
+  }
+  
+  onChangeCurrentLang(code: LanguageCodes): void {
+    console.log(code)
+    localStorage.setItem('languageCode', code);
+    this.translate.use(code);
   }
 
 }
